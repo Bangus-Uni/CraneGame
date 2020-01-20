b97d95c66d5405b472e9943d998cec43429ff3dc
Ben Angus <u1725266@uel.ac.uk>
2020-01-20 15:05:12 +0000

    Started Character Controller

---
+++ Crane Game/Assets/Scripts/Crane.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crane : MonoBehaviour
+{
+
+    public float flSpeed;
+    public float flRotRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0) {
+            float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime * flRotRate;
+            transform.Rotate(0, speed, 0); 
+        }
+    }
+}
