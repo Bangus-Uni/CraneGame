54c99def4a7248e37b5998f39839d2381fa0fd6b
Ben Angus <u1725266@uel.ac.uk>
2020-01-20 17:55:18 +0000

    Created Character controller and started work on chain

--- Crane Game/Assets/Scripts/Crane.cs
+++ Crane Game/Assets/Scripts/Crane.cs
@@ -4,22 +4,48 @@ using UnityEngine;
 
 public class Crane : MonoBehaviour
 {
+    private Rigidbody rbPC;
 
     public float flSpeed;
     public float flRotRate;
+    public float flRotRateTrue;
+    public float flSpeedTrue;
+
+    public bool boolThrust = true;
+    public bool boolRotEnabled = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rbPC = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Horizontal") != 0) {
-            float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime * flRotRate;
-            transform.Rotate(0, speed, 0); 
+        flRotRateTrue = flRotRate;
+        flSpeedTrue = flSpeed;
+        boolThrust = true;
+        boolRotEnabled = true;
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            flRotRateTrue = flRotRate * 5;
+            boolThrust = false;
+        }
+
+        else if (Input.GetKey(KeyCode.Space)) {
+            flSpeedTrue = flSpeed * 5;
+            boolRotEnabled = false;
+        }
+
+        if (Input.GetAxis("Horizontal") != 0 && boolRotEnabled) {
+            float _flRotation = Input.GetAxisRaw("Horizontal") * Time.deltaTime * flRotRateTrue;
+            transform.Rotate(0, _flRotation, 0); 
+        }
+
+        if(Input.GetAxis("Vertical") != 0 && boolThrust) {
+            float _flSpeed = Input.GetAxisRaw("Vertical") * Time.deltaTime * flSpeedTrue;
+            rbPC.velocity = transform.forward * _flSpeed;
         }
     }
 }
